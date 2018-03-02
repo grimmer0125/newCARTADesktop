e176ee4ca5af26d48eaeb54eff73c2bcf894aa4d
Anthony Moraghan <ajm@asiaa.sinica.edu.tw>
2018-03-02 10:21:37 +0800

    This script file is no longer needed

--- docker-start-linux.sh
+++
@@ -1,4 +0,0 @@
-echo "Running the docker-start-linux.sh script"
-echo "\$DISPLAY is" $DISPLAY
-docker run -p 3000:3000 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  ajmasiaa/newcarta_meteor_v2 /start.sh
-~  
